58d4b7dc51f4906d67b1fb0098b884a7fc26f0e7
priyankapeelu <priyankapeelu@icloud.com>
2022-05-12 15:50:50 +0530

    terraform variablescd tt

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "terraformd63"
-    key    = "alb/${var.ENV}/terraform.tfstate"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
 }
