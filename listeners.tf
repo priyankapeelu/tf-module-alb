4998cace01c7cddd4c6bb5953c84fb3a9c97a1d7
priyankapeelu <priyankapeelu@icloud.com>
2022-05-12 15:52:54 +0530

    terraform variablescd tt

--- listeners.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "terraformd63"
-    key    = "vpc/${var.ENV}/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "ALB_ARN" {
   value = aws_lb.alb.arn
 }
 
-#output "LISTENER_ARN" {
-#  value = aws_lb_listener.private.*.arn
-#}
+output "LISTENER_ARN" {
+  value = aws_lb_listener.private.*.arn
+}
 
 output "ALB_ADDRESS" {
   value = aws_lb.alb.dns_name
